9deb8b382eff8314bc578062002feff1af7aaeaf
Kyle Czajkowski <skiroyjenkins@gmail.com>
2019-01-10 21:47:29 -0700

    trying to add concepts to model...

--- packr_model/model_controllers.js
+++ packr_model/model_controllers.js
@@ -1,15 +1,62 @@
 require('dotenv').config()
 const Clarifai = require('clarifai')
 const app = new Clarifai.App({apiKey: process.env.API_KEY})
+const knex = require('../db/connection')
+
+const addItem = (response) => {
+    if(response.inputs > 1){
+        const body = {name: req.body.name, nfc: response.inputs[0].id}
+        return knex('item')
+            .insert(body)
+            .returning('*') 
+            .then(item => res.json({item: item[0]}))  
+    }
+}
 
 const train = (req, res, next) => {
 
 }
 
 const addConceptsWithImages = (req, res, next) => {
-    app.inputs.create({
-
+    // console.log('hey')
+    app.models.initModel('packr')
+    .then(packr=>{
+        console.log(packr)
+        return packr.inputs.create({
+            url: 'https://i.imgur.com/rUjAgch.jpg',
+            id: 'creator',
+            concepts:[{
+                id: 'creator',
+                value: true
+            }]
+            }
+        )
     })
+        // .then(packr => {
+        //     console.log(packr)
+            // if(req.body.length === 1){
+                 
+                .then(response => res.json({response}))
+        //     } else {
+        //         const array = req.body.img.reduce((accum, item, i) => {
+        //             let newObject = new Object()
+        //             newObject['base64'] = item
+        //             newObject['id'] = req.body.name
+        //             accum.push(newObject)
+        //             return accum
+        //         },[])
+        //         return packr.inputs.create({array})
+        //         .then(response => console.log(response))
+        //     }
+        // // })
+        // .then(response => console.log(response))
+        .catch(err => {
+            console.log('add concepts with images err:',err)
+            return res.json({error: err})
+        })
+    
+
+    
 }
 
 const addConcepts = (req, res, next) => {
@@ -18,4 +65,5 @@ const addConcepts = (req, res, next) => {
 
 
 module.exports = {
+    addConceptsWithImages
 }
--- packr_model/model_routes.js
+++ packr_model/model_routes.js
@@ -2,5 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('./model_controllers')
 
+router.get('/create', controller.addConceptsWithImages) 
 
 module.exports = router
