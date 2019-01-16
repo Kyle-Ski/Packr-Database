600c1b310eefa72c1c65e9b9f6bac09f97844a20
Kyle Czajkowski <skiroyjenkins@gmail.com>
2019-01-15 17:46:37 -0700

    capitalized seed names

--- seeds/02_item.js.js
+++ seeds/02_item.js.js
@@ -5,17 +5,17 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('item').insert([
-        {id: 1, name: 'tent', nfc: ''},
-        {id: 2, name: 'stove', nfc: ''},
-        {id: 3, name: 'knife', nfc: ''},
-        {id: 4, name: 'sleeping bag', nfc: ''},
-        {id: 5, name: 'first aid', nfc: ''},
-        {id: 6, name: 'fuel', nfc: ''},
-        {id: 7, name: 'sleeping pad', nfc: ''},
-        {id: 8, name: 'hat', nfc: ''},
-        {id: 9, name: 'boots', nfc: ''},
-        {id: 10, name: 'food', nfc: ''},
-        {id: 11, name: 'code', nfc: ''},
+        {id: 1, name: 'Tent', nfc: ''},
+        {id: 2, name: 'Stove', nfc: ''},
+        {id: 3, name: 'Knife', nfc: ''},
+        {id: 4, name: 'Sleeping bag', nfc: ''},
+        {id: 5, name: 'First Aid', nfc: ''},
+        {id: 6, name: 'Fuel', nfc: ''},
+        {id: 7, name: 'Sleeping Pad', nfc: ''},
+        {id: 8, name: 'Hat', nfc: ''},
+        {id: 9, name: 'Boots', nfc: ''},
+        {id: 10, name: 'Food', nfc: ''},
+        {id: 11, name: 'Code', nfc: ''},
         {id: 12, name: 'water', nfc: ''}
       ]);
     });
